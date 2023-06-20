b416453b5bdab8ae0c7db37d792c3bbb15007e45
unlimit97 <44885126+unlimit97@users.noreply.github.com>
2023-06-20 12:46:29 +0900

    Update pol.sh

--- pol.sh
+++ pol.sh
@@ -2,6 +2,19 @@
  
 echo 'Running Coverity on Polaris'
 
+echo 'polaris_instanceurl'
+echo ${polaris_instanceurl}
+export POLARIS_SERVER_URL=${polaris_instanceurl}
+
+echo ''
+echo 'polaris_authtoken'
+echo ${polaris_authtoken}
+export POLARIS_ACCESS_TOKEN=${polaris_authtoken}
+
+echo ''
+echo 'polaris_projectName'
+echo ${polaris_projectName}
+
 polaris setup
 polaris --co project.name=${polaris_projectName} analyze -w
 
