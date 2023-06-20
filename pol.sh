52e7bb16d2387c4d7f9fe8d1d6782edb9aa6dd3b
unlimit97 <44885126+unlimit97@users.noreply.github.com>
2023-06-20 16:02:19 +0900

    Update pol.sh

--- pol.sh
+++ pol.sh
@@ -3,13 +3,13 @@
 echo 'Running Coverity on Polaris'
 
 echo 'polaris_instanceurl'
-echo ${polaris_instanceurl}
-export POLARIS_SERVER_URL=${polaris_instanceurl}
+echo ${polaris_instanceUrl}
+export POLARIS_SERVER_URL=${polaris_instanceUrl}
 
 echo ''
 echo 'polaris_authtoken'
-echo ${polaris_authtoken}
-export POLARIS_ACCESS_TOKEN=${polaris_authtoken}
+echo ${polaris_authToken}
+export POLARIS_ACCESS_TOKEN=${polaris_authToken}
 
 echo ''
 echo 'polaris_projectName'
